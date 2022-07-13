11d25ee5d00a25fb2ffac29f157575ed10436c95
s1l3nt-k0d3r <s1l3nt.k0d3r@gmail.com>
2022-07-13 12:26:22 -0400

    ApiTable component 3 files modified, 3 added, 3 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
+import { MatTableModule } from '@angular/material/table';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatPaginatorModule } from '@angular/material/paginator';
+
 import { AppComponent } from './app.component';
-import { ApiTableComponent } from './components/api_table/api_table.component';
+import { ApiTableComponent } from './components/apiTable/apiTable.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
+  imports:      [ BrowserModule, 
+                  FormsModule, 
+                  MatProgressSpinnerModule, 
+                  MatTableModule,
+                  MatPaginatorModule ],
   declarations: [ AppComponent, ApiTableComponent ],
   bootstrap:    [ AppComponent ]
 })
---
+++ src/app/components/apiTable/apiTable.component.ts
@@ -0,0 +1,50 @@
+import { Component, Input, AfterViewInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { MatTableDataSource } from '@angular/material/table';
+import { Observable, Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+
+export interface ToDoItem {
+  userId: number;
+  id: number;
+  title: string;
+  completed: boolean;
+}
+
+@Component({
+  selector: 'apiTable'
+  , templateUrl: './apiTable.component.html'
+  , styleUrls: [ './apiTable.component.css' ]
+})
+export class ApiTableComponent implements AfterViewInit {
+  @Input() name: string;
+  
+  public displayedColumns: string[] = ['userid', 'id', 'title', 'completed', 'action'];
+  public tableData = new MatTableDataSource();
+
+  public unsubscribe$: Subject<void> = new Subject();
+
+  resultsLength = 0;
+  isLoadingResults = true;
+
+  constructor(private httpClient: HttpClient) {}
+
+  ngAfterViewInit() {
+      this.isLoadingResults = true;
+
+      this.getToDoItems()
+          .pipe(takeUntil(this.unsubscribe$))
+          .subscribe((apiData: any) => {
+              this.tableData = apiData;
+
+              this.resultsLength = apiData.length;
+          });
+
+      this.isLoadingResults = false;
+  }
+
+  getToDoItems(): Observable<ToDoItem> {
+      return this.httpClient.get<ToDoItem>('https://jsonplaceholder.typicode.com/todos');
+  }
+}
